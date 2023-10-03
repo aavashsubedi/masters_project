c3cfe6ccb784a2ec5caa36613260008df0c8e368
LIslaam <labeebah.islaam@gmail.com>
2023-10-03 15:34:28 +0100

    dijkstra

--- src/model/dijkstra.py
+++ src/model/dijkstra.py
@@ -8,39 +8,6 @@ def hamming_loss(suggested, target):
 
     return errors.mean(dim=0).sum()
 
- 
-    # Function that implements Dijkstra's single source
-    # shortest path algorithm for a graph represented
-    # using adjacency matrix representation
-    def dijkstra(self, src):
- 
-        dist = [1e7] * self.V
-        dist[src] = 0
-        sptSet = [False] * self.V
- 
-        for cout in range(self.V):
- 
-            # Pick the minimum distance vertex from
-            # the set of vertices not yet processed.
-            # u is always equal to src in first iteration
-            u = self.minDistance(dist, sptSet)
- 
-            # Put the minimum distance vertex in the
-            # shortest path tree
-            sptSet[u] = True
- 
-            # Update dist value of the adjacent vertices
-            # of the picked vertex only if the current
-            # distance is greater than new distance and
-            # the vertex in not in the shortest path tree
-            for v in range(self.V):
-                if (self.graph[u][v] > 0 and
-                   sptSet[v] == False and
-                   dist[v] > dist[u] + self.graph[u][v]):
-                    dist[v] = dist[u] + self.graph[u][v]
- 
-        self.printSolution(dist)
-
 
 class Graph:
     def __init__(self, vertices):
