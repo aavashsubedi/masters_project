7cee2471befacd7b81ab636e910cd697688b557f
Aavash Subedi <87420856+aavashsubedi@users.noreply.github.com>
2023-10-03 13:49:16 +0000

    Added dataloader

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 import torch
-from src.preprocessing.download_data import 
+from src.dataset.dataloader import get_dataloader
 def run(cfg):
-    download_url()
+    train_dataloader = get_dataloader(cfg, mode="train")
+    for x in train_dataloader:
+        data, label = x[0], x[1]
+    val_dataloader = get_dataloader(cfg, mode="val")
+    test_dataloader = get_dataloader(cfg, mode="test")
+
+    import pdb; pdb.set_trace()
+    pass    
