Fixed the Djisktra code. working on gradients

src/model/combinatorial_solvers.py
@@ -3,31 +3,38 @@ import torch.nn as nn
 import torch.optim as optim
 import heapq
 import itertools
-from graph.py import neighbours_8
+from .graph import neighbours_8
 from functools import partial
 import numpy as np
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # Put on every file
-
-
 class Dijkstra(nn.Module): # Dijkstra algorithm is a combinatorial solver to find shortest paths
     def __init__(self):
         super(Dijkstra, self).__init__()
         self.output = namedtuple("DijkstraOutput", ["shortest_path", "is_unique", "transitions"])
 
-    def forward(self, matrix, neighbourhood_fn=neighbours_8, request_transitions=False):
-        with torch.no_grad():
-            x_max, y_max = matrix.shape
-            neighbors_func = partial((neighbourhood_fn), x_max=x_max, y_max=y_max)
+    def forward(self, matrices, neighbourhood_fn=neighbours_8, request_transitions=False):
+        # Ensure matrices is a 4D tensor (batch_size, channels, height, width)
+        matrices = torch.tensor(matrices)
+        batch_size, height, width = matrices.size()
+        outputs = []
+        if matrices.dim() != 3:
+            raise ValueError("Input matrices must be a 3D tensor (batch_size, height, width).")
 
-            costs = np.full_like(matrix, 1.0e10)
+
+        for batch_idx in range(batch_size):
+            matrix = matrices[batch_idx]  # Extract the current matrix
+            x_max, y_max = matrix.shape
+            neighbors_func = partial(neighbourhood_fn, x_max=x_max, y_max=y_max)
+
+            costs = torch.full_like(matrix, 1.0e10)
             costs[0][0] = matrix[0][0]
-            num_path = np.zeros_like(matrix)
+            num_path = torch.zeros_like(matrix)
             num_path[0][0] = 1
             priority_queue = [(matrix[0][0], (0, 0))]
             certain = set()
-            transitions = dict()
+            transitions = defaultdict(tuple)
 
             while priority_queue:
                 cur_cost, (cur_x, cur_y) = heapq.heappop(priority_queue)
@@ -45,9 +52,10 @@ class Dijkstra(nn.Module): # Dijkstra algorithm is a combinatorial solver to fin
                             num_path[x, y] += 1
 
                 certain.add((cur_x, cur_y))
-            # retrieve the path
+            
+            # Retrieve the path
             cur_x, cur_y = x_max - 1, y_max - 1
-            on_path = np.zeros_like(matrix)
+            on_path = torch.zeros_like(matrix)
             on_path[-1][-1] = 1
             while (cur_x, cur_y) != (0, 0):
                 cur_x, cur_y = transitions[(cur_x, cur_y)]
@@ -56,9 +64,70 @@ class Dijkstra(nn.Module): # Dijkstra algorithm is a combinatorial solver to fin
             is_unique = num_path[-1, -1] == 1
 
             if request_transitions:
-                return self.output(shortest_path=on_path, is_unique=is_unique, transitions=transitions)
+                outputs.append(self.output(shortest_path=on_path.unsqueeze(0), is_unique=is_unique, transitions=transitions))
             else:
-                return self.output(shortest_path=on_path, is_unique=is_unique, transitions=None)
+                outputs.append(self.output(shortest_path=on_path.unsqueeze(0), is_unique=is_unique, transitions=None))
+
+        shortest_paths = []
+        for output in outputs:
+            shortest_path = output.shortest_path
+            is_unique = output.is_unique
+            transitions = output.transitions
+            shortest_paths.append(shortest_path)
+       # import pdb; pdb.set_trace()
+        return torch.cat(shortest_paths, dim=0)
+# class Dijkstra(nn.Module): # Dijkstra algorithm is a combinatorial solver to find shortest paths
+#     def __init__(self):
+#         super(Dijkstra, self).__init__()
+#         self.output = namedtuple("DijkstraOutput", ["shortest_path", "is_unique", "transitions"])
+
+#     def forward(self, matrix, neighbourhood_fn=neighbours_8, request_transitions=False):
+#         """
+#         The input of this should be [x, y] and the output from cnn = [b, x, y]
+
+#         """
+#         with torch.no_grad():
+#             x_max, y_max = matrix.shape
+#             neighbors_func = partial((neighbourhood_fn), x_max=x_max, y_max=y_max)
+
+#             costs = np.full_like(matrix, 1.0e10)
+#             costs[0][0] = matrix[0][0]
+#             num_path = np.zeros_like(matrix)
+#             num_path[0][0] = 1
+#             priority_queue = [(matrix[0][0], (0, 0))]
+#             certain = set()
+#             transitions = dict()
+
+#             while priority_queue:
+#                 cur_cost, (cur_x, cur_y) = heapq.heappop(priority_queue)
+#                 if (cur_x, cur_y) in certain:
+#                     pass
+
+#                 for x, y in neighbors_func(cur_x, cur_y):
+#                     if (x, y) not in certain:
+#                         if matrix[x][y] + costs[cur_x][cur_y] < costs[x][y]:
+#                             costs[x][y] = matrix[x][y] + costs[cur_x][cur_y]
+#                             heapq.heappush(priority_queue, (costs[x][y], (x, y)))
+#                             transitions[(x, y)] = (cur_x, cur_y)
+#                             num_path[x, y] = num_path[cur_x, cur_y]
+#                         elif matrix[x][y] + costs[cur_x][cur_y] == costs[x][y]:
+#                             num_path[x, y] += 1
+
+#                 certain.add((cur_x, cur_y))
+#             # retrieve the path
+#             cur_x, cur_y = x_max - 1, y_max - 1
+#             on_path = np.zeros_like(matrix)
+#             on_path[-1][-1] = 1
+#             while (cur_x, cur_y) != (0, 0):
+#                 cur_x, cur_y = transitions[(cur_x, cur_y)]
+#                 on_path[cur_x, cur_y] = 1.0
+
+#             is_unique = num_path[-1, -1] == 1
+
+#             if request_transitions:
+#                 return self.output(shortest_path=on_path, is_unique=is_unique, transitions=transitions)
+#             else:
+#                 return self.output(shortest_path=on_path, is_unique=is_unique, transitions=None)
 
 
 #combinatorial_solver = Dijkstra()

src/model/model.py
@@ -6,34 +6,48 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torch.nn.functional as F
-from .loss import HammingLoss
+from src.utils.loss import HammingLoss
 from math import sqrt
-from combinatorial_solvers import Dijkstra
+from .combinatorial_solvers import Dijkstra
+
+def glorot_initializer(shape):
+    # Calculate the scale factor for Glorot initialization
+    fan_in, fan_out = shape[0], shape[1]
+    scale = torch.sqrt(torch.tensor(2.0 / (fan_in + fan_out), dtype=torch.float32))
+    
+    # Generate random values with a uniform distribution
+    return torch.rand(shape, dtype=torch.float32) * 2 * scale - scale
 
 class CombRenset18(nn.Module):
 
     def __init__(self, out_features, in_channels):
+        """
+        Expected shape is [b, 3, h, w]
+        """
         super().__init__()
         self.resnet_model = torchvision.models.resnet18(pretrained=False, num_classes=out_features)
         del self.resnet_model.conv1
-        self.resnet_model.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.resnet_model.conv1 = nn.Conv2d(in_channels,
+         64, kernel_size=7, stride=2, padding=3, bias=False)
         output_shape = (int(sqrt(out_features)), int(sqrt(out_features)))
         self.pool = nn.AdaptiveMaxPool2d(output_shape)
         #self.last_conv = nn.Conv2d(128, 1, kernel_size=1,  stride=1)
 
 
     def forward(self, x):
-        x = self.resnet_model.conv1(x)
+
+        x = self.resnet_model.conv1(x) #64, 48, 48
+
         x = self.resnet_model.bn1(x)
         x = self.resnet_model.relu(x)
-        x = self.resnet_model.maxpool(x)
+        x = self.resnet_model.maxpool(x) #64, 64, 24
         x = self.resnet_model.layer1(x)
         #x = self.resnet_model.layer2(x)
         #x = self.resnet_model.layer3(x)
         #x = self.last_conv(x)
         x = self.pool(x)
         x = x.mean(dim=1)
-        return x
+        return x #shape is 32, 12, 12
     
 class GradientApproximator():
 
@@ -49,33 +63,33 @@ class GradientApproximator():
         self.labels = None
         self.cnn_loss = None
 
-    def forward_pass(self, input):
+    def forward_pass(self, cnn_input):
         if self.prev_cnn_input == None:
             #take the shape of the input and create a tensor of random numbers with the same shape
-            self.prev_cnn_input = torch.rand(input.shape)
+            self.prev_cnn_input = torch.rand(cnn_input.shape)
 
-        self.cnn_input = input.detach().cpu().numpy()
+        self.cnn_input = cnn_input.detach().cpu().numpy()
         self.combinatorial_output = self.combinatorial_solver(self.cnn_input)
         self.prev_cnn_input = self.output
 
     def backward_pass(self):
         #input to the backward pass is from the forward pass before djikstra and after djikstra
         loss = self.loss(self.combinatorial_output, self.labels)
+        self.combinatorial_output.require_grad = True
+        self.labels.require_grad = True
         loss_grad = loss.detach().grad
-
+        import pdb; pdb.set_trace()
         perturbed_cnn_weights   = self.prev_cnn_input + self.lambda_val * loss_grad
         perturbed_cnn_output    = self.combinatorial_solver(perturbed_cnn_weights)
         new_grads = -(1/self.lambda_val) * (perturbed_cnn_output - self.combinatorial_output)
         return new_grads
-    def propogate(self, input, labels):
+    def propogate(self, cnn_input, labels):
         self.labels = labels
-        self.forward_pass(input)
+        self.forward_pass(cnn_input)
         grads = self.backward_pass()
-        with torch.no_grad():
-            for param, grad in zip(self.model.parameters(), grads):
-                param -= grad
-        return self.backward_pass()
-    
+
+        # for name, param in self.model.named_parameters():
+        #     import pdb; pdb.set_trace()
 
 
 
@@ -83,26 +97,26 @@ def get_model(cfg, warcraft_experiment=True):
     #if we use warcraft experiment I will set the default parameters from the paper
 
     if warcraft_experiment:
-        resnet_module = CombRenset18(out_features=144, in_channels=96)
+        resnet_module = CombRenset18(out_features=144, in_channels=3)
         return resnet_module
 
 
 
-def forward_pass(input, solver=dijkstra): # Include this fn in the architecture of the model
-    input = input.detach().cpu().numpy()
-    output = solver(input) # Inputs for dijkstra algo
-    log_input_output(input, output) # decide how to save params!!
+# def forward_pass(input, solver=dijkstra): # Include this fn in the architecture of the model
+#     input = input.detach().cpu().numpy()
+#     output = solver(input) # Inputs for dijkstra algo
+#     log_input_output(input, output) # decide how to save params!!
     
-    return output # What is the correct form for CNN?
+#     return output # What is the correct form for CNN?
 
-def backward_pass(grad, lambda_val, solver=dikkstra): # Include this fn in the architecture of the model
-    input, output = load_input_output() # from forward pass after applying cc. Output: 
-    input += param * grad
-    perturbed_output = solver(input)
+# def backward_pass(grad, lambda_val, solver=dikkstra): # Include this fn in the architecture of the model
+#     input, output = load_input_output() # from forward pass after applying cc. Output: 
+#     input += param * grad
+#     perturbed_output = solver(input)
 
-    gradient = -(1/lambda_val) * (perturbed_output - output)
+#     gradient = -(1/lambda_val) * (perturbed_output - output)
 
-    return gradient.to(device)
+#     return gradient.to(device)
 
 # class CNNModel(nn.Module):
 #     def __init__(self, cfg):

src/training/trainer.py
@@ -1,17 +1,19 @@
 from src.training.optimizers import get_optimizer, get_scheulder_one_cycle, get_flat_scheduler
-from src.utils.loss import hamming_loss
+from src.utils.loss import HammingLoss
 import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 from tqdm import tqdm
 from src.utils.visualise_gradients import plot_grad_flow
 import wandb
+from src.model.model import GradientApproximator
 
 
 def trainer(cfg, train_dataloader, val_dataloader,
             test_dataloader, model):
     optimizer = get_optimizer(cfg, model)
-    criterion = hamming_loss()
+    criterion = HammingLoss()
     model.train().to(device)
+    gradient_approximater = GradientApproximator(model)
     
     if cfg.scheduler:
         scheduler = get_scheulder_one_cycle(cfg, optimizer, len(train_dataloader), cfg.epochs)
@@ -26,12 +28,14 @@ def trainer(cfg, train_dataloader, val_dataloader,
         pbar_data = tqdm(train_dataloader, desc=f"Epoch {epoch}",
                          leave=False)
         for data in pbar_data:
-            data = data.to(device)
+            #import pdb; pdb.set_trace()
+            data, label = data
             output = model(data)
-
-            loss = criterion(output, data)
+            gradients = gradient_approximater.propogate(output,
+                                                        label)
+            #loss = criterion(output, data)
             optimizer.zero_grad()
-            loss.backward()
+            #loss.backward()
             optimizer.step()
             scheduler.step()
             pbar_data.set_postfix(loss=loss.item())

src/utils/loss.py
@@ -1,4 +1,18 @@
+import torch
 
-def hamming_loss(suggested, target):
-    errors = suggested * (1.0 - target) + (1.0 - suggested) * target
-    return errors.mean(dim=0).sum()
+class HammingLoss(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, prediction, targets):
+        errors = prediction * (1.0 - targets) + (1.0 - prediction) * targets
+        return errors.mean(dim=0).sum()
+
+# loss = HammingLoss()
+# prediction = torch.tensor([[0.5, 0.5, 0.5, 0.5]])
+# target = torch.tensor([[1.0, 0.0, 1.0, 0.0]])
+# print(loss(prediction, target))
+
+
+# class ConcreteDropout(torch.nn.module):
+#     def __init(self):
+#         pass