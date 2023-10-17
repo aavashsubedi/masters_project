Fixed computation graph

src/model/model.py
@@ -8,7 +8,7 @@ import torchvision
 import torch.nn.functional as F
 from src.utils.loss import HammingLoss
 from math import sqrt
-from .combinatorial_solvers import Dijkstra
+from .combinatorial_solvers import Dijkstra, DijskstraClass
 
 def glorot_initializer(shape):
     # Calculate the scale factor for Glorot initialization
@@ -39,7 +39,8 @@ class CombRenset18(nn.Module):
         output_shape = (int(sqrt(out_features)), int(sqrt(out_features)))
         self.pool = nn.AdaptiveMaxPool2d(output_shape)
         #self.last_conv = nn.Conv2d(128, 1, kernel_size=1,  stride=1)
-
+        self.combinatorial_solver = DijskstraClass.apply
+        self.grad_approx = GradientApproximator.apply
 
     def forward(self, x):
         x = self.resnet_model.conv1(x) #64, 48, 48
@@ -53,11 +54,15 @@ class CombRenset18(nn.Module):
         #x = self.last_conv(x)
         x = self.pool(x)
         x = x.mean(dim=1)
+        cnn_output = x.abs()
+        combinatorial_solver_output = self.combinatorial_solver(cnn_output)
+        x = self.grad_approx(combinatorial_solver_output, cnn_output)
         return x #shape is 32, 12, 12
     
 
 class GradientApproximator(torch.autograd.Function):
-    def __init__(self, model, input_shape):
+    def __init__(self, model, input_shape,
+                 lambda_val=0.1):
         self.input = None
         self.output = None
         self.prev_cnn_input = torch.rand(input_shape)
@@ -65,36 +70,43 @@ class GradientApproximator(torch.autograd.Function):
         self.curr_output = None
         self.lambda_val = 0.1
         self.model = model
-        self.criterion = HammingLoss().requires_grad_(True)
-        self.combinatorial_solver = Dijkstra()
+        #self.criterion = HammingLoss().requires_grad_(True)
+        #self.combinatorial_solver = DijskstraClass()
         self.labels = None
         self.cnn_loss = None
 
     @staticmethod
-    def forward(ctx, cnn_input, labels):
+    def forward(ctx, combinatorial_solver_output, cnn_output,
+                ):
         #ctx.criterion = HammingLoss().requires_grad_(True)
 
-        ctx.labels = labels
-        ctx.cnn_input = cnn_input
+        #ctx.labels = labels
+        #ctx.cnn_input = cnn_input
         
         #this line here is the issue, there is no connection between the cnn_input and the combinatorial solver
-        ctx.combinatorial_output = ctx.combinatorial_solver(cnn_input)
-        ctx.loss = ctx.criterion(ctx.combinatorial_output, ctx.labels)
-        print("doing the forward pass")
+        #combinatorial_output = ctx.combinatorial_solver(cnn_input)
+        #loss = ctx.criterion(combinatorial_output, ctx.labels)
+        #print("doing the forward pass")
         #import pdb; pdb.set_trace()
-        return ctx.combinatorial_output
+        ctx.save_for_backward(combinatorial_solver_output, cnn_output)
+        return combinatorial_solver_output
     
     @staticmethod
-    def backward(ctx): # Deviation from paper algo, calculate grad in function
-        import pdb; pdb.set_trace()
-
-        ctx.loss_grad = torch.autograd.grad(ctx.loss, ctx.combinatorial_output)
-       # import pdb ; pdb.set_trace()
-        perturbed_cnn_weights = ctx.cnn_input + torch.matmul(torch.full(ctx.cnn_input.shape, 0.1), ctx.loss_grad[0]) # Is this variable named accurately?
-        perturbed_cnn_output = ctx.combinatorial_solver(perturbed_cnn_weights)
-        new_grads = -(1/ctx.lambda_val) * (perturbed_cnn_output - ctx.combinatorial_solver(ctx.cnn_input))
-       # import pdb; pdb.set_trace()
-        return new_grads
+    def backward(ctx, grad_input,
+                 ): # Deviation from paper algo, calculate grad in function
+        #shape of loss grad is [1, 32, 12, 12]
+       # combinatorial_solver = DijskstraClass()
+        #import pdb; pdb.set_trace()
+        lambda_val = 0.1
+        combinatorial_solver_output, cnn_output = ctx.saved_tensors
+        perturbed_cnn_weights = cnn_output + torch.matmul(torch.full(cnn_output.shape, 0.1), grad_input[0]) # Is this variable named accurately?
+        perturbed_cnn_output = DijskstraClass.apply(perturbed_cnn_weights)
+        new_grads = -(1 / lambda_val) * (combinatorial_solver_output - perturbed_cnn_output)
+        return new_grads, new_grads
+        # perturbed_cnn_output = combinatorial_solver(perturbed_cnn_weights)
+        # new_grads = -(1 / ctx.lambda_val) * (combinatorial_solver_output - perturbed_cnn_output)
+    
+        # return new_grads
     
 
 

src/training/trainer.py
@@ -6,8 +6,9 @@ from tqdm import tqdm
 from src.utils.visualise_gradients import plot_grad_flow
 import wandb
 from src.model.model import GradientApproximator
+from src.model.combinatorial_solvers import Dijkstra, DijskstraClass
 from torchviz import make_dot
-
+from copy import deepcopy
 
 
 
@@ -28,6 +29,7 @@ def trainer(cfg, train_dataloader, val_dataloader,
     criterion = HammingLoss()
     model.train().to(device)
     gradient_approximater = GradientApproximator(model, input_shape=(cfg.batch_size, 12, 12))
+   # dijs = DijskstraClass()
     
     if cfg.scheduler:
         scheduler = get_scheulder_one_cycle(cfg, optimizer, len(train_dataloader), cfg.epochs)
@@ -37,39 +39,65 @@ def trainer(cfg, train_dataloader, val_dataloader,
     curr_epoch = 0
     pbar_epochs = tqdm(range(cfg.num_epochs), desc="Pretraining",
                         leave=False)
+    
+    data_copy = None
+    label_copy = None
     for epoch in pbar_epochs:
 
         pbar_data = tqdm(train_dataloader, desc=f"Epoch {epoch}",
                          leave=False)
         wandb.watch(model)
-
+        i = 0
         for data in pbar_data:
+            # if data_copy != None:
+            #     #skip the loop
+            #     continue
+            if i == 0:
+                if data_copy == None:
+                    data, label = data
+                    data_copy = deepcopy(data)
+                    label_copy = deepcopy(label) 
+                data, label  = data_copy, label_copy
+                # import pdb; pdb.set_trace()
+                i += 1
+            else:
+                continue
+
             #import pdb; pdb.set_trace()
-            data, label = data
+            #data, label = data
             output = model(data)
-            abs_output = output.abs() #not sure if this workls
+            #import pdb; pdb.set_trace()
+            loss = criterion(output, label)
+            loss.backward()
+            #abs_output = output.abs() #not sure if this workls
             #loss = test_fn(abs_output)
             #loss = criterion(abs_output, label)
-            import pdb; pdb.set_trace()
-            gradient_approximater.forward(gradient_approximater,
-                                                            output, label) # Used forward instead of apply() for GPU friendliness
-            gradient_approximater.backward()
-            #shortest_path.backward()
 
+            #import pdb; pdb.set_trace()
+            #output = gradient_approximater.forward(gradient_approximater,
+            #                                                output, label) # Used forward instead of apply() for GPU friendliness
+            #output.backward()
+            #new_gradients = gradient_approximater.backward(gradient_approximater)
+            #shortest_path.backward()
+            #abs_output.backward(new_gradients)
             #gradient_approximater.backward(gradient_approximater)
             #this is just doing the hamming loss it doesnt do anything else.
-            loss = gradient_approximater.criterion(shortest_path, label)
+            #loss = criterion(abs_output.detach(), label).detach()
+
             #simport pdb; pdb.set_trace()
-            optimizer.zero_grad()
+            #optimizer.zero_grad()
             #loss.backward()
             optimizer.step()
             scheduler.step()
             pbar_data.set_postfix(loss=loss.item())
             plot_grad_flow(model.named_parameters())
+
             #dot = make_dot(loss, params=dict(model.named_parameters()))
             #import pdb; pdb.set_trace() 
+            
             #uncessary at the moment
             # torch.nn.utils.clip_grad_norm_(model.parameters(),
             #                                 cfg.gradient_clipping)
             wandb.log({"loss": loss.item()})
 
+            #data_copy = deepcopy(data)