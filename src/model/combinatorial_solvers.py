fixed GNN gradients

src/model/combinatorial_solvers.py
@@ -93,7 +93,7 @@ def DijkstraGraph(x, graph, target=122):
     #set the path nodes to 1
     path_tensor[path] = 1
 
-    return path_tensor
+    return path_tensor.requires_grad_(True).to(device)
 
 
 class DijkstraGraphClass(torch.autograd.Function):
@@ -101,15 +101,17 @@ class DijkstraGraphClass(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x, graph):
         ctx.graph = graph
-        result = DijkstraGraph(x, graph) # Input must be a graph
+        result = DijkstraGraph(x, graph,
+                               target=x.shape[0] - 1) # Input must be a graph
         return result
     
     @staticmethod
     def backward(ctx, grad_output):
         #input_ = ctx.saved_tensors
         #why are we doing this?
-         
-        return grad_output #* input_[0]
+        #grad_output.shape = [123]
+        grad_output = grad_output.unsqueeze(-1)
+        return grad_output, None #* input_[0]
 
 ####################################################### Spectral Clustering on graphs
 

src/model/graph_model.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch_geometric.nn import GINEConv, GCNConv
 from torch_geometric.nn import global_max_pool
 from .combinatorial_solvers import DijkstraGraph, DijkstraGraphClass
+import copy
 
    
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -109,11 +110,13 @@ class WarCraftModel(torch.nn.Module):
         x = torch.nn.ReLU()(x)
         x = self.conv3(x, edge_index, edge_attr.float())
         #x = global_max_pool(x, data.batch) # This might not work (issues with shape?)
+        #gradients have issues here. 
+        #x.shape here is [123, 1]. so maybe we unsqueeze here
 
         combinatorial_solver_output = self.combinatorial_solver(x, data)
         x = self.grad_approx(combinatorial_solver_output, x, data)
 
-        return x, data 
+        return x 
 
 
 def get_graph_model(cfg, warcraft=True):
@@ -132,11 +135,10 @@ def test_model():
     model.to(device)
     data = torch.load("masters_project/data/graph_dataset.pt")
     output = model(data)
-    import pdb; pdb.set_trace()
 
     
 class GradApproxGraph(torch.autograd.Function):
-    def __init__(self, model, input_data, lambda_val=0.1,
+    def __init__(self, model, input_data, lambda_val=20,
                  example_input=None):
         """
         input_data here is a graph with the correct edges. and setup.
@@ -165,19 +167,15 @@ class GradApproxGraph(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_input):
-        lambda_val = 0.1
+        lambda_val = 20
         combinatorial_solver_output, x = ctx.saved_tensors
         graph = ctx.graph
 
         perturbed_gnn_weights = x + torch.multiply(lambda_val, grad_input.to(device))
         perturbed_gnn_output = DijkstraGraphClass.apply(perturbed_gnn_weights, graph)
-
         new_grads = - (1 / lambda_val) * (combinatorial_solver_output - perturbed_gnn_output)
-
-        # Create an artificial gradient for x
-        dummy_loss = new_grads.sum().requires_grad_()  # Define a dummy loss
-        fake_grad_x = torch.zeros_like(x) #torch.autograd.grad(dummy_loss, x, create_graph=True, allow_unused=True)[0]
-
-        import pdb; pdb.set_trace()
         
-        return new_grads, fake_grad_x, None
+        new_grads_2 = copy.deepcopy(new_grads)
+        new_grads_2.requires_grad_(True).unsqueeze_(-1)
+        
+        return new_grads, new_grads_2, None

src/model/model.py
@@ -114,7 +114,7 @@ class GradientApproximator(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_input): # Deviation from paper algo, calculate grad in function
-        """
+    
         # Linear interpolation
         combinatorial_solver_output, cnn_output = ctx.saved_tensors
         lambda_val = 20
@@ -124,9 +124,8 @@ class GradientApproximator(torch.autograd.Function):
 
         new_grads = -(1 / lambda_val) * (combinatorial_solver_output - perturbed_cnn_output)
         import pdb; pdb.set_trace()
-
-        """
         
+        """
         # Concrete Dropout interpolation
         eps = 1e-7
         tmp = 0.1
@@ -145,6 +144,7 @@ class GradientApproximator(torch.autograd.Function):
         retain_prob = 1 - ctx.p
 
         new_grads = - torch.mul(grad_input, random_tensor) / retain_prob # TESTING MULTIPLICATION BY 20
+        """
 
         #wandb.log({"abs-mean-new-gradients": torch.mean(torch.abs(new_grads[0])).item()})
         

src/training/trainer_graph.py
@@ -55,39 +55,20 @@ def trainer_graph(cfg, train_dataloader, val_dataloader,
         wandb.watch(model)
         i = 0
         for data in pbar_data:
+            optimizer.zero_grad()
             
-            # if data_copy != None:
-            #     #skip the loop
-            #     continue
-            # if i == 0:
-            #     if data_copy == None:
-            #         data, label, weights = data
-            #         data_copy = deepcopy(data)
-            #         label_copy = deepcopy(label) 
-            #         weights_copy = deepcopy(weights)
-            #     data, label, weights  = data_copy, label_copy, weights_copy
-            #     # import pdb; pdb.set_trace()
-            #     i += 1
-            # else:
-            #     continue
+
             data = data.to(device) #label, weights = data
             label = data.centroid_in_path.to(device)
-            #weights.to(device)
             
-            output, graph = model(data)
+            output = model(data)
             output = output.to(device)
-            #import pdb; pdb.set_trace()
-
+            
             loss = criterion(output, label)
             loss.backward()
 
             optimizer.step()
             scheduler.step()
-            pbar_data.set_postfix(loss=loss.item())
-           # plot_grad_flow(model.named_parameters())
-
-            #dot = make_dot(loss, params=dict(model.named_parameters()))
-            #import pdb; pdb.set_trace() 
             
             #uncessary at the moment
             # torch.nn.utils.clip_grad_norm_(model.parameters(),