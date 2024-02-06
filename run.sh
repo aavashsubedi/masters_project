6a4dce8eaece0984c6f7f498e17fba05e065bbce
Labeebah Islaam <lislaam@galahad.ast.man.ac.uk>
2024-02-06 12:24:20 +0000

    shell

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=CombLearning
+#SBATCH --job-name=CombinatorialRL
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 #SBATCH --nodes=1
@@ -9,3 +9,8 @@
 #SBATCH --constraint=A100
 #SBATCH --exclusive
 
+nvidia-smi 
+echo ">>>start"
+source /lislaam/masters_project/venv/bin/activate
+echo ">>>training"
+python /lislaam/masters_project/src/rl_pipeline/run_experiment.py
